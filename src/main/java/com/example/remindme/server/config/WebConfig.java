c814bcbb902b626f3c34804bcbd91dae432a7f4d
AlbertKobiakov <sdf15SDS666BvZZ>
2017-11-24 01:21:59 +0300

    Configure base project and create Controller

---
+++ src/main/java/com/example/remindme/server/config/WebConfig.java
@@ -0,0 +1,4 @@
+package com.example.remindme.server.config;
+
+public class WebConfig {
+}
---
+++ src/main/java/com/example/remindme/server/controller/ReminderController.java
@@ -0,0 +1,13 @@
+package com.example.remindme.server.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/remind")
+public class RemindController {
+
+    public String getReminder(){
+        return "";
+    }
+}
